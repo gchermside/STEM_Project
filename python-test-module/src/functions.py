0f6b42c5865b7b26b04674200685d51f10077a44
Genevieve <genchermside@gmail.com>
2022-02-22 17:34:12 -0500

    Sam made cosSimilarity Function

--- python-test-module/src/functions.py
+++ python-test-module/src/functions.py
@@ -1,3 +1,8 @@
+import array
+import numpy
+import math
+from numpy import array
+
 # this function points moves the hand so the wrist is at 0,0 and makes it a consistant
 # porportion(the length between point 0 and 1 will always be the same on hands)
 def regularize(hand):
@@ -32,6 +37,45 @@ def compareHands(hand1, hand2):
         # for i in range(0, 4):
         #     difTotal = difTotal + abs(hand1T[i]-hand2T[i])
     return difTotal
+
+
+
+#cos similarity between two vectors(hands)
+def cosSimilarity(hand1, hand2):
+    # a*b=||a||*||b||*cos(theta)
+    # CS = (a*b)/(||a||*||b||)
+    # print("hi")
+    # a = array([])
+    # b = array([])
+    gen1 = []
+    gen2 = []
+    for i in range(0,21):
+        for g in range(0,3):
+            gen1.append(hand1.landmarks[i][g])
+            # a += hand1.landmarks[i][g]
+    for i in range(0,21):
+        for g in range(0,3):
+            gen2.append(hand2.landmarks[i][g])
+            # b += hand2.landmarks[i][g]
+    a = array(gen1)
+    b = array(gen2)
+    c = a * b
+    top = 0
+    for y in c:
+        top += y
+    # print(f"Top is {top}")
+    la = 0
+    for j in a:
+        la += j*j
+    la = math.sqrt(la)
+    lb = 0
+    for j in b:
+        lb += j*j
+    lb = math.sqrt(lb)
+    bottom = la*lb
+    # print(f"Bottom is {bottom}")
+    cs = (top/bottom)
+    return cs
 
 
 # this function points moves the hand so the wrist is at 0,0 and makes it a consistant
