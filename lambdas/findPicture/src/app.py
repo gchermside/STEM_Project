a43ba88cc72ba91a54bb783f8d97e5bab289acba
Michael Chermside <mcherm@mcherm.com>
2022-03-29 21:12:41 -0400

    Finished getting the first version working that actually parses files in a lambda.

--- lambdas/findPicture/src/app.py
+++ lambdas/findPicture/src/app.py
@@ -8,18 +8,22 @@ def lambda_handler(event, context):
     global model
 
     print(f"the event is {event}")
-    landmarks = event['body']
-    print(landmarks)
+    landmarks = json.loads(event['body'])
+    print("landmarks", landmarks)
 
     # Load pickled model from file and unpickle, if it isn't already loaded
     if model is None:
         with open("picture.pkl", 'rb') as f:
             model = pickle.load(f)
 
+    predictions = model.predict([landmarks])
+    print("predictions:", predictions)
+    prediction = predictions[0]
+
 
     return {
         'statusCode': 200,
-        'body': json.dumps({"bestGuess": "g"}),
+        'body': json.dumps({"bestGuess": prediction}),
         'headers': {
             "Access-Control-Allow-Origin" : "*",
         }
