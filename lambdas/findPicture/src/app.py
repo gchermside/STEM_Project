875281ef125e131d4874d6e096322e25674dfa33
Michael Chermside <mcherm@mcherm.com>
2022-03-29 20:54:31 -0400

    Make the findPicture lambda use sklearn.

--- lambdas/findPicture/src/app.py
+++ lambdas/findPicture/src/app.py
@@ -1,18 +1,25 @@
 import json
-from library import two
+import pickle
+import sklearn
+
+model = None
 
 def lambda_handler(event, context):
-    # TODO implement
+    global model
 
     print(f"the event is {event}")
     landmarks = event['body']
     print(landmarks)
-    print(two())
+
+    # Load pickled model from file and unpickle, if it isn't already loaded
+    if model is None:
+        with open("picture.pkl", 'rb') as f:
+            model = pickle.load(f)
 
 
     return {
         'statusCode': 200,
-        'body': json.dumps({"bestGuess": "f"}),
+        'body': json.dumps({"bestGuess": "g"}),
         'headers': {
             "Access-Control-Allow-Origin" : "*",
         }
